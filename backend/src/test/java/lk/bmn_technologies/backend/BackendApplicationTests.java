1242314bc6c5de035bcfa59b04304ee6c61f65b4
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2025-04-30 01:27:59 +0530

    refactor: no changes

---
+++ backend/src/test/java/lk/bmn_technologies/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.bmn_technologies.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
