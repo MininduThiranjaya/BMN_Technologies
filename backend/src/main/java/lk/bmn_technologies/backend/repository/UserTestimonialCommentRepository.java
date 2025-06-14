817bdf8d87310d97616a3926654b10c6c19931e0
MininduThiranjaya <thiranjaya2017@gmail.com>
2025-06-14 10:10:30 +0530

    feat: create UserTestimonialCommentModel

--- backend/src/main/java/lk/bmn_technologies/backend/repository/UserTestimonialCommentRepository.java
+++ backend/src/main/java/lk/bmn_technologies/backend/repository/UserTestimonialCommentRepository.java
@@ -1,6 +1,10 @@
 package lk.bmn_technologies.backend.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import lk.bmn_technologies.backend.model.UserTestimonialCommentModel;
@@ -8,4 +12,6 @@ import lk.bmn_technologies.backend.model.UserTestimonialCommentModel;
 @Repository
 public interface UserTestimonialCommentRepository extends JpaRepository<UserTestimonialCommentModel, Long>{
 
+    @Query("select t from UserTestimonialCommentModel t")
+    public List<UserTestimonialCommentModel> getUserTestimonialComment(Pageable pageable);
 }
