f2471ee5603a031d8973ec5850bfa86f2edfdbcf
MininduThiranjaya <thiranjaya2017@gmail.com>
2025-06-14 10:14:06 +0530

    feat: create AuthenticationServiceImpl

---
+++ backend/src/main/java/lk/bmn_technologies/backend/services/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,77 @@
+package lk.bmn_technologies.backend.services.impl;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lk.bmn_technologies.backend.services.AuthenticationService;
+
+@Service
+public class AuthenticationServiceImpl implements  AuthenticationService{
+
+    @Override
+    public UserDetails validateToken(String token) {
+        String userName = extractUserName(token);
+        return userDetailsService.loadUserByUsername(userName);
+    }
+
+    private String extractUserName(String token) {
+        Claims claims = Jwts.parser()
+            .setSigningKey(getSigningKey())
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
+        return claims.getSubject();
+    }
+
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsService userDetailsService;
+    private final long jwtExpiry = 86400000L;
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    public AuthenticationServiceImpl(AuthenticationManager authenticationManager, UserDetailsService userDetailsService) {
+        this.authenticationManager = authenticationManager;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    public UserDetails authenticate(String email, String password) {
+        
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+        UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+        return userDetails;
+    }
+
+    @Override
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return Jwts.builder()
+            .setClaims(claims)
+            .setSubject(userDetails.getUsername())
+            .setIssuedAt(new Date(System.currentTimeMillis()))
+            .setExpiration(new Date(System.currentTimeMillis() * jwtExpiry))
+            .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+            .compact();
+    }
+
+    private Key getSigningKey() {
+
+        byte[] keyBites = secretKey.getBytes();
+        return Keys.hmacShaKeyFor(keyBites);
+    }
+
+}
