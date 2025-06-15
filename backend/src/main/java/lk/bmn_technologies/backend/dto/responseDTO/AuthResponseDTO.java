9770ccd766777ff5f4c02e21d547d4492ef9af29
chamithjeewantha <chamithjeewa123@gmail.com>
2025-06-15 23:36:49 +0530

    feat: update attributes of AuthResponseDTO

--- backend/src/main/java/lk/bmn_technologies/backend/dto/responseDTO/AuthResponseDTO.java
+++ backend/src/main/java/lk/bmn_technologies/backend/dto/responseDTO/AuthResponseDTO.java
@@ -4,22 +4,48 @@ public class AuthResponseDTO {
 
     private String token;
     private long expiresIn;
+    private boolean success;
+    private Object user;
 
-    public AuthResponseDTO(String token, long expiresIn) {
-        this.expiresIn = expiresIn;
+    
+    public AuthResponseDTO(String token, long expiresIn, Object user, boolean success) {
         this.token = token;
+        this.expiresIn = expiresIn;
+        this.user = user;
+        this.success = success;
     }
 
+    public Object getUser() {
+        return user;
+    }
+
+    public void setUser(Object user) {
+        this.user = user;
+    }
+    
     public String getToken() {
         return token;
     }
+
     public void setToken(String token) {
         this.token = token;
     }
+
     public long getExpiresIn() {
         return expiresIn;
     }
+
     public void setExpiresIn(long expiresIn) {
         this.expiresIn = expiresIn;
     }
+
+    public boolean issuccess() {
+        return success;
+    }
+
+    public void setsuccess(boolean success) {
+        this.success = success;
+    }
+
+    
 }
