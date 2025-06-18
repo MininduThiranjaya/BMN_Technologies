9728c1008af6ed48534b4ea804899939f4c25ecf
chamithjeewantha <chamithjeewa123@gmail.com>
2025-06-18 10:26:54 +0530

    feat: create product model

---
+++ backend/src/main/java/lk/bmn_technologies/backend/model/ProductModel.java
@@ -0,0 +1,79 @@
+package lk.bmn_technologies.backend.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class ProductModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String productId;
+    private String productName;
+    private String productDescription;
+    private Double productPrice;
+    private String category;
+    @OneToMany(mappedBy = "product")
+    private List<ProductImageModel> imageUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public Double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(Double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public List<ProductImageModel> getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(List<ProductImageModel> imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
