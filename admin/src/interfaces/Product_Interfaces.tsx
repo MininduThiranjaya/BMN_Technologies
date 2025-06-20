30b9e0ce4475d0b5275f393082e6278c0702c7b6
MininduThiranjaya <thiranjaya2017@gmail.com>
2025-06-21 00:36:39 +0530

    feat: create new product interfaces

--- admin/src/interfaces/Product_Interfaces.tsx
+++ admin/src/interfaces/Product_Interfaces.tsx
@@ -1,14 +1,58 @@
 export interface ImageType {
   file: File | null;
-  preview: string | null;
+  preview: string | null | undefined;
   name: string | null;
 }
 
 export interface AddProductProps {
-  isOpen: boolean;
+  existFormData: ProductType | null;
+  isOpen: boolean | null;
   onClose: () => void;
+  type: string;
+  title: string;
+  statement: string;
+  onSuccess: () => void;
 }
 
 export interface ProductImage {
   imageUrl: String;
+}
+
+export interface ProductType {
+    id: number | null;
+    productId: string | null;
+    productName: string | null;
+    imageUrl: ProductImages[] | [];
+    productDescription: string | null;
+    productPrice: number | null;
+    category: string | null;
+}
+
+interface ProductImages {
+  id: number;
+  imageUrl: string;
+}
+
+export interface ProductDetailsProps {
+  products : ProductType[];
+  deleteProduct: (id: number) => void;
+  onSuccess: () => void;
+}
+
+export interface formDataType {
+  productId: string | null | undefined,
+  productName: string | null | undefined,
+  productPrice: number | null | undefined,
+  productDescription: string | null | undefined,
+  category: string | null | undefined,
+}
+
+export interface DeleteConfirmationType {
+  product: ProductType | null;
+  cancelDelete: () => void;
+  confirmDelete: () => void;
+}
+
+export interface ProductPropsType {
+  onSuccess: () => void;
 }
