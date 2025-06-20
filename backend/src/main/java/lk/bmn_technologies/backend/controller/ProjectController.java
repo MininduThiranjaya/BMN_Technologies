0102bc115c0c44dab0e7f7ecd2eb6ae2d72279a3
chamithjeewantha <chamithjeewa123@gmail.com>
2025-06-20 06:28:00 +0530

    feat: create get and count conroller for  project

--- backend/src/main/java/lk/bmn_technologies/backend/controller/ProjectController.java
+++ backend/src/main/java/lk/bmn_technologies/backend/controller/ProjectController.java
@@ -1,11 +1,15 @@
 package lk.bmn_technologies.backend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lk.bmn_technologies.backend.dto.responseDTO.ProjectDTO;
 import lk.bmn_technologies.backend.model.ProjectModel;
 import lk.bmn_technologies.backend.services.ProjectService;
 
@@ -19,5 +23,15 @@ public class ProjectController {
     @PostMapping("/add")
     public void addProjectIntoDatabase(@RequestBody ProjectModel data) {
         service.addProject(data);
+    }
+
+    @GetMapping("/get")
+    public List<ProjectDTO> getProductsFromDatabase() {
+        return service.getProduct();
+    }
+
+    @GetMapping("/count")
+    public long countProductsInDatabase() {
+        return service.countProducts();
     }
 }
