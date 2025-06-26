71652a25c43e97f17e2db0388ed1f769df4b4225
chamithjeewantha <chamithjeewa123@gmail.com>
2025-06-26 17:12:10 +0530

    feat: create project image repo

---
+++ backend/src/main/java/lk/bmn_technologies/backend/repository/ProjectImageRepo.java
@@ -0,0 +1,16 @@
+package lk.bmn_technologies.backend.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import lk.bmn_technologies.backend.model.ProjectImageModel;
+
+@Repository
+public interface ProjectImageRepo extends JpaRepository<ProjectImageModel, Long>{
+
+    @Query(value = "SELECT REPLACE(SUBSTRING_INDEX(SUBSTRING_INDEX(p.image_url, '/upload/', -1), '/', -3), '.png', '') AS publicId FROM project_image_model p WHERE p.project_id = ?1", nativeQuery = true)
+    public List<String> getProjectImages(Long id);
+}
