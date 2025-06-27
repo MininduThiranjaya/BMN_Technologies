afb7e61075f0ab08618af30769dac995852ab540
MininduThiranjaya <thiranjaya2017@gmail.com>
2025-06-27 23:11:05 +0530

    fix: fix getting images from repos

--- backend/src/main/java/lk/bmn_technologies/backend/repository/ProductImageRepo.java
+++ backend/src/main/java/lk/bmn_technologies/backend/repository/ProductImageRepo.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import lk.bmn_technologies.backend.model.ProductImageModel;
 
+@Repository
 public interface ProductImageRepo extends JpaRepository<ProductImageModel, Long>{
 
-    @Query(value = "SELECT REPLACE(SUBSTRING_INDEX(SUBSTRING_INDEX(p.image_url, '/upload/', -1), '/', -3), '.png', '') AS publicId FROM product_image_model p WHERE p.product_id = ?1", nativeQuery = true)
+    @Query(value = "SELECT REPLACE(REPLACE(SUBSTRING_INDEX(SUBSTRING_INDEX(p.image_url, '/upload/', -1), '/', -3), '.png', ''), '.jpg', '') AS publicId FROM product_image_model p WHERE p.product_id = ?1", nativeQuery = true)
     public List<String> getProductImages(Long id);
 }
