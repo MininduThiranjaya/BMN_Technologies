ac966b89e1b27bd26949c07708e5d14cd14293d7
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2025-10-05 23:48:25 +0530

    feat: create initial route path

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+import "./index.css";
+import "./App.css";
+import Home from "./page/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+export default App;
