0349b8ed7cad83d80cc7d52dd994d7e1f9cd6d82
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2025-10-06 19:43:32 +0530

    feat: create product and project repo methods

--- backend/src/main/java/lk/bmn_technologies/backend/repository/ProjectRepo.java
+++ backend/src/main/java/lk/bmn_technologies/backend/repository/ProjectRepo.java
@@ -1,12 +1,22 @@
 package lk.bmn_technologies.backend.repository;
 
+import java.sql.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import lk.bmn_technologies.backend.dto.responseDTO.ProjectDTO;
 import lk.bmn_technologies.backend.model.ProjectModel;
 
 @Repository
 public interface ProjectRepo extends JpaRepository<ProjectModel, Long> {
-    // Define any custom query methods if needed
-
+    
+    @Query("SELECT p FROM ProjectModel p " +
+    "WHERE (:category IS NULL OR p.category = :category) " +
+    "AND (:location IS NULL OR p.location = :location) " +
+    "AND (:projectMinDate IS NULL OR p.projectDate >= :projectMinDate) " +
+    "AND (:projectMaxDate IS NULL OR p.projectDate <= :projectMaxDate)")
+    public List<ProjectDTO> getFilteredProjects(String category, String location, Date projectMinDate, Date projectMaxDate);
 }
