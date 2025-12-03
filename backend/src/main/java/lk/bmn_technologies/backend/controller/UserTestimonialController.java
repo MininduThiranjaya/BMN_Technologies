ab0230959c412c878efb1ede07970c0b066efd0e
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2025-12-03 11:52:19 +0530

    feat: add count change state and get all testimonial control

--- backend/src/main/java/lk/bmn_technologies/backend/controller/UserTestimonialController.java
+++ backend/src/main/java/lk/bmn_technologies/backend/controller/UserTestimonialController.java
@@ -1,9 +1,12 @@
 package lk.bmn_technologies.backend.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import lk.bmn_technologies.backend.dto.ApiResponseDTO;
 import lk.bmn_technologies.backend.model.UserTestimonialCommentModel;
 import lk.bmn_technologies.backend.services.UserTestimonialService;
+
 
 
 
@@ -47,4 +51,37 @@ public class UserTestimonialController {
     }
     
     
+    @GetMapping("count")
+    public long countUserTestimonialInDatabase() {
+        return service.countUserTestimonial();
+    }
+
+    @PutMapping("change-state")
+    public ResponseEntity<ApiResponseDTO> deleteProductFromDatabase(@RequestBody Map<String, Long> body) {
+        long id  = body.get("id");
+        try{
+            String response = service.changeState(id);
+            return ResponseEntity
+                .status(200)
+                .body(new ApiResponseDTO(true, "Changed status" + response));
+        }
+        catch(Exception e) {
+            return ResponseEntity
+                .status(500)
+                .body(new ApiResponseDTO(false, "Error deleting product: " + e.getMessage()));
+        }
+    }
+
+    @GetMapping("get-all")
+    public ResponseEntity<ApiResponseDTO> getAllTestimonialComment() {
+        try {
+            ApiResponseDTO response = service.getAllTestimonialCommentService();
+            return ResponseEntity.ok(response);
+        }
+        catch (Exception e) {
+            return ResponseEntity
+                .status(500)
+                .body(new ApiResponseDTO(false, "Error saving data: " + e.getMessage()));
+        }
+    }
 }
