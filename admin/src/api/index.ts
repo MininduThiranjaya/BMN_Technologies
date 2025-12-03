30aadc0122b850a72c79c20e16778c69b449ba93
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2025-12-04 03:28:03 +0530

    feat: add fiter and get action end points

--- admin/src/api/index.ts
+++ admin/src/api/index.ts
@@ -13,17 +13,20 @@ export const endpoints = {
     count: `${BACKEND_BASE_URL}auth/product/count`,
     getAllProducts: `${BACKEND_BASE_URL}auth/product/get/all`,
     deleteProduct: `${BACKEND_BASE_URL}auth/product/delete-by-id/:productId`,
+    getFiltered: `${BACKEND_BASE_URL}auth/product/get/filter`
   },
   project: {
     add: `${BACKEND_BASE_URL}auth/project/add`,
     count: `${BACKEND_BASE_URL}auth/project/count`,
     getAllProjects: `${BACKEND_BASE_URL}auth/project/get/all`,
     deleteProject: `${BACKEND_BASE_URL}auth/project/delete-by-id/:projectId`,
+    getFiltered: `${BACKEND_BASE_URL}auth/project/get/filter`
   },
   contactUs: {
     getIssues: `${BACKEND_BASE_URL}user-contact/get-issues`,
     getAllIssues: `${BACKEND_BASE_URL}user-contact/get-all-issues`,
     count: `${BACKEND_BASE_URL}user-contact/count`,
+    getAction: `${BACKEND_BASE_URL}user-contact/get-action`,
   },
   testimonial: {
     get: `${BACKEND_BASE_URL}user-testimonial/get-all`,
