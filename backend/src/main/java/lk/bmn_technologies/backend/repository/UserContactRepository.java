39d6f08add7293f0740421146b2929a80df618f7
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2025-12-03 11:59:47 +0530

    feat: add query for get all user issues and make user issues read

--- backend/src/main/java/lk/bmn_technologies/backend/repository/UserContactRepository.java
+++ backend/src/main/java/lk/bmn_technologies/backend/repository/UserContactRepository.java
@@ -2,16 +2,25 @@ package lk.bmn_technologies.backend.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import jakarta.transaction.Transactional;
 import lk.bmn_technologies.backend.model.UserContactModel;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 @Repository
 public interface UserContactRepository extends JpaRepository<UserContactModel, Long>{
     
-    @Query("SELECT u FROM UserContactModel u WHERE u.isAvailable = true")
+    @Query("SELECT u FROM UserContactModel u WHERE u.isAvailable = 1")
     public List<UserContactModel> getAvailableUserIssues();
+
+    @Query("SELECT u FROM UserContactModel u ORDER BY u.createdAt DESC")
+    public List<UserContactModel> getAllUserIssues();
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE UserContactModel u SET u.isAvailable = 0 WHERE u.isAvailable = 1")
+    public int makeUserIssuesRead();
 }
