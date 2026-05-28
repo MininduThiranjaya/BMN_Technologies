69c3cea0d2a1527dc0fb471fe2a1a857c4deb495
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-05-28 10:05:45 +0530

    fix: resolve project filtering

--- backend/src/main/java/lk/bmn_technologies/backend/repository/ProjectRepo.java
+++ backend/src/main/java/lk/bmn_technologies/backend/repository/ProjectRepo.java
@@ -7,16 +7,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import lk.bmn_technologies.backend.dto.responseDTO.ProjectDTO;
 import lk.bmn_technologies.backend.model.ProjectModel;
 
 @Repository
 public interface ProjectRepo extends JpaRepository<ProjectModel, Long> {
     
     @Query("SELECT p FROM ProjectModel p " +
-    "WHERE (:category IS NULL OR p.category = :category) " +
-    "AND (:location IS NULL OR p.location = :location) " +
-    "AND (:projectMinDate IS NULL OR p.projectDate >= :projectMinDate) " +
-    "AND (:projectMaxDate IS NULL OR p.projectDate <= :projectMaxDate)")
-    public List<ProjectDTO> getFilteredProjects(String category, String location, Date projectMinDate, Date projectMaxDate);
+     "WHERE (:category IS NULL OR :category = 'all' OR p.category = :category) " +
+     "AND (:province IS NULL OR p.province = :province) " +
+     "AND (:projectMinDate IS NULL OR p.projectDate >= :projectMinDate) " +
+     "AND (:projectMaxDate IS NULL OR p.projectDate <= :projectMaxDate)")
+    public List<ProjectModel> getFilteredProjects(String category, String province, Date projectMinDate, Date projectMaxDate);
 }
