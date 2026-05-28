80684d0e621eb81bc3073929222f7eb106de2118
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-05-28 10:14:42 +0530

    fix: resolve project filtering

--- web/src/interfaces/Project.ts
+++ web/src/interfaces/Project.ts
@@ -2,7 +2,7 @@ export interface ProjectItemType {
     projectId: string,
     projectName: string,
     personName: string,
-    location: string,
+    province: string,
     projectDescription: string,
     category: string,
     projectDate: string,
@@ -11,7 +11,7 @@ export interface ProjectItemType {
 
 export interface ProjectFilter {
     category: string | null,
-    location: string | null,
+    province: string | null,
     projectMinDate: string | null,
     projectMaxDate: string | null,
 }
