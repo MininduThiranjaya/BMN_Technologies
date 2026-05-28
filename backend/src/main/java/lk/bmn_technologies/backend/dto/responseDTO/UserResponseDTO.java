bafa147cd10ee813574abd4782a8beedfa61c483
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-05-28 16:39:17 +0530

    feat: update dto attribute

---
+++ backend/src/main/java/lk/bmn_technologies/backend/dto/responseDTO/UserResponseDTO.java
@@ -0,0 +1,84 @@
+package lk.bmn_technologies.backend.dto.responseDTO;
+
+import java.time.LocalDateTime;
+
+public class UserResponseDTO {
+    private long id;
+    private String email;
+    private String userName;
+    private String role;
+    private int phoneNumber;
+    private LocalDateTime lastLogin;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private boolean isSuspended;
+
+    public UserResponseDTO(long id, String email, String userName, String role, int phoneNumber, LocalDateTime lastLogin, LocalDateTime updatedAt, LocalDateTime createdAt, boolean isSuspended) {
+        this.id = id;
+        this.email = email;
+        this.userName = userName;
+        this.role = role;
+        this.phoneNumber = phoneNumber;
+        this.isSuspended = isSuspended;
+        this.lastLogin = lastLogin;
+        this.updatedAt = updatedAt;
+        this.createdAt = createdAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public LocalDateTime getLastLogin() {
+        return lastLogin;
+    }
+    public void setLastLogin(LocalDateTime lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public boolean isSuspended() {
+        return isSuspended;
+    }
+    public void setIsSuspended(boolean isSuspended) {
+        this.isSuspended = isSuspended;
+    }
+}
