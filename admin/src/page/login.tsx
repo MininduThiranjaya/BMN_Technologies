bd17721681b6c41ba4c7dcb098658e56a60757ca
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-05-28 10:14:02 +0530

    feat: add company name

--- admin/src/page/login.tsx
+++ admin/src/page/login.tsx
@@ -58,6 +58,9 @@ export default function LoginPage() {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <header className="absolute top-0 text-center w-full p-4 ">
+        <h1 className="text-5xl font-bold text-gray-800">BMN Technologies Admin</h1>
+      </header>
       <div className="max-w-md w-full bg-white p-8 shadow-lg rounded-lg">
         <h2 className="text-3xl font-bold text-center mb-6">Sign In</h2>
         <form onSubmit={handleSubmit} className="space-y-5">
