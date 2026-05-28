80d08b3f98bf1bc6c61055be60903aa4d6732430
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-05-28 16:44:47 +0530

    feat: add admin user interface

--- admin/src/interfaces/Common_Interfaces.ts
+++ admin/src/interfaces/Common_Interfaces.ts
@@ -15,3 +15,15 @@ export interface UserIssue {
   createdAt: string | number,
   action: number 
 }
+
+export interface AdminUser {
+  id: number ,
+  userName: string | null,
+  phoneNumber: string | null,
+  email: string | null,
+  lastLogin: Date | null,
+  createdAt: Date | null,
+  updatedAt: Date | null,
+  role: string | number,
+  suspended: boolean | null
+}
