545017989228ba3b319b9caf4e92d78d97623ea3
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-09-07 00:48:31 +0530

    feat: product interface update

--- web_v1/src/interfaces/Product.ts
+++ web_v1/src/interfaces/Product.ts
@@ -11,3 +11,37 @@ export interface ProductFilter {
   minPrice: number | null;
   maxPrice: number | null;
 }
+
+export type ProductCategory =
+  | "solar"
+  | "on_grid_inverter"
+  | "off_grid_inverter"
+  | "hybrid_inverter"
+  | "on_grid_battery"
+  | "off_grid_battery"
+  | "hybrid_battery";
+
+export interface ProductImage {
+  id: number;
+  imageUrl: string;
+}
+export interface Product {
+  id: number;
+  productId: string;
+  productName: string;
+  productDescription: string;
+  productPrice: number;
+  category: ProductCategory;
+  images: ProductImage[];
+  createdAt: string;
+  updatedAt: string;
+}
+
+
+export interface ProductsPageResponse {
+  items: Product[];
+  currentPage: number;
+  pageSize: number;
+  totalItems: number;
+  totalPages: number;
+}
